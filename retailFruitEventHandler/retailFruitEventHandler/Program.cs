74a3432af4f3030605478c1815d5217a8d6b12a9
RobvanEmbden <61789232+RobvanEmbden@users.noreply.github.com>
2021-03-07 17:40:42 +0100

    Add files via upload

---
+++ retailFruitEventHandler/retailFruitEventHandler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace retailFruitEventHandler
+{
+    class Program
+    {// event handlers
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Fruit();
+            fruit.NewEvent += new EventHandler(newKind);// create new event and launch method 'newKind'
+            fruit.Kind = "Apple";
+            fruit.Kind = "Banana";
+            fruit.Kind = "Orange";
+            fruit.Kind = "Cherry";
+        }
+        static void newKind(object newk, EventArgs e)
+        {
+            Fruit fruit = (Fruit)newk;
+            Console.WriteLine("Fruit changed to {0}", fruit.Kind);
+        }
+        class Fruit
+        {
+            public event EventHandler NewEvent;
+            private string kind;
+            public string Kind
+            {
+                get { return kind; }
+                set 
+                {
+                    kind = value;
+                    NewEvent(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
